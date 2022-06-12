a7060f9823eed26e2f28b9edd2edcbf43d5bcd6a
plusls <plusls@qq.com>
2022-06-12 19:47:33 +0800

    better minimum-operations-to-reduce-x-to-zero

--- minimum-operations-to-reduce-x-to-zero/src/main.rs
+++ minimum-operations-to-reduce-x-to-zero/src/main.rs
@@ -1,35 +1,30 @@
 impl Solution {
     pub fn min_operations(nums: Vec<i32>, x: i32) -> i32 {
-        use std::collections::HashMap;
         use std::cmp::min;
         // println!("nums: {:?}, x: {}", nums, x);
-        let mut sums: Vec<i32> = Vec::new();
+        let target = nums.iter().sum::<i32>() - x;
+        let mut l = 0;
+        let mut r = 0;
+        let len = nums.len();
         let mut sum = 0;
-        let mut sums_idx_map: HashMap<i32, i32> = HashMap::new();
-        sums_idx_map.insert(0, -1);
-        for num in &nums {
-            sum += num;
-            sums.push(sum);
-            sums_idx_map.insert(sum, sums.len() as i32 - 1);
-        }
-        let mut result = i32::MAX;
-        // println!("sums: {:?}", sums);
-        for i in (0..nums.len()).rev() {
-            let n = x - (sum - sums[i]);
-            if let Some(idx) = sums_idx_map.get(&n) {
-                let idx = *idx;
-                if idx > i as i32 {
-                    continue;
-                }
-                result = min(result, nums.len() as i32 - 1 - i as i32 + idx + 1);
-                // println!("left: {}, right: {}", idx + 1, nums.len() - 1 - i);
+        let mut ret = i32::MAX;
+        while r < len {
+            sum += nums[r];
+            while sum > target && l <= r {
+                sum -= nums[l];
+                l += 1;
             }
+            if sum == target {
+                ret = min(ret, (len - (r + 1 - l)) as i32);
+            }
+            // println!("l:{}, r:{}, sum:{}, target:{}", l, r, sum, target);
+            r += 1;
         }
 
-        if result == i32::MAX {
+        if ret == i32::MAX {
             -1
         } else {
-            result
+            ret
         }
     }
 }
