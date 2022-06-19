a27af876154617190b438a38bd76b82bd6c8e710
plusls <plusls@qq.com>
2022-06-19 17:15:31 +0800

    prefix-and-suffix-search use box

--- prefix-and-suffix-search/src/main.rs
+++ prefix-and-suffix-search/src/main.rs
@@ -1,31 +1,26 @@
-use std::cell::RefCell;
-use std::collections::hash_map::Entry;
 use std::collections::HashMap;
-use std::rc::Rc;
 
 #[derive(Debug)]
 struct Node {
-    next: HashMap<u8, Rc<RefCell<Node>>>,
+    next: HashMap<u8, Box<Node>>,
     max_idx: usize,
 }
 
 #[derive(Debug)]
 struct WordFilter {
-    words: Rc<RefCell<Node>>,
-    count: usize,
+    words: Box<Node>,
 }
 
 
 impl WordFilter {
     fn new(words: Vec<String>) -> Self {
         // let mut ret = Self { words_tree: Box::new(TreeNode { next: HashMap::new(), max_idx: usize::MAX }) };
-        let ret = Self {
-            words: Rc::new(RefCell::new(
+        let mut ret = Self {
+            words: Box::new(
                 Node {
                     next: HashMap::new(),
                     max_idx: usize::MAX,
-                })),
-            count: words.len(),
+                }),
         };
         for (i, word) in words.iter().enumerate() {
             let new_word = word.clone() + "#" + &*word;
@@ -36,45 +31,30 @@ impl WordFilter {
         ret
     }
 
-    fn insert(&self, word: &[u8], idx: usize) {
+    fn insert(&mut self, word: &[u8], idx: usize) {
         // println!("insert: {:?}", String::from_utf8(Vec::from(word)));
-        let mut current_node = self.words.clone();
-        for ch in word {
-            match current_node.clone().borrow_mut().next.entry(*ch) {
-                Entry::Occupied(mut occupied) => {
-                    let occupied = occupied.get_mut();
-                    occupied.borrow_mut().max_idx = idx;
-                    current_node = occupied.clone();
-                }
-                Entry::Vacant(vacant) => {
-                    current_node = vacant.insert(Rc::new(RefCell::new(
-                        Node {
-                            next: HashMap::new(),
-                            max_idx: idx,
-                        }))).clone();
-                }
-            }
-            // println!("insert: {}, idx: {}", *ch as char, idx);
-        }
+        word.iter().fold(&mut self.words, |node, &ch| {
+            let next_node = node.next.entry(ch).or_insert_with(|| Box::new(Node {
+                next: HashMap::new(),
+                max_idx: usize::MAX,
+            }));
+            next_node.max_idx = idx;
+            next_node
+        });
+        // println!("insert: {}, idx: {}", *ch as char, idx);
     }
 
     fn f(&self, prefix: String, suffix: String) -> i32 {
         let s = suffix + "#" + &*prefix;
 
-        let mut current_node = self.words.clone();
-        let mut ret = self.count as i32;
-        for ch in s.as_bytes() {
-            if let Some(next_node) = current_node.clone().borrow().next.get(ch) {
-                ret = next_node.borrow().max_idx as i32;
-                current_node = next_node.clone();
-            } else {
-                ret = -1;
-                break;
+        s.as_bytes().iter().try_fold(&self.words, |node, &ch| {
+            match node.next.get(&ch) {
+                Some(node) => Ok(node),
+                None => Err(()),
             }
-            // println!("search: {}, ret: {}", *ch as char, ret);
-        }
-        // println!("ret: {}", ret);
-        ret
+        }).map_or(-1, |node| {
+            node.max_idx as i32
+        })
     }
 }
 
@@ -90,4 +70,5 @@ fn main() {
     word_filter.f("b".to_string(), "".to_string());
     word_filter.f("bbabaa".to_string(), "baab".to_string());
     word_filter.f("bbabaab".to_string(), "baab".to_string());
+    word_filter.f("".to_string(), "".to_string());
 }
