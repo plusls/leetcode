44445d22d373855cfb66f300b34456ba2450ba46
plusls <plusls@qq.com>
2022-07-06 14:50:09 +0800

    add fibonacci-number

---
+++ fibonacci-number/src/main.rs
@@ -0,0 +1,20 @@
+impl Solution {
+    pub fn fib(n: i32) -> i32 {
+        let result = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040];
+        result[n as usize]
+    }
+}
+
+struct Solution;
+
+
+fn main() {
+    use std::io;
+    loop {
+        let mut input = String::new();
+        if io::stdin().read_line(&mut input).unwrap() == 0 {
+            break;
+        }
+        println!("{:?}", Solution::fib(input.trim().parse().unwrap()));
+    }
+}
